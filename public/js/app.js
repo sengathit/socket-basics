093cb9fb007f00c11b593db06475929b54ffd158
sengathit <sengathit.l@gmail.com>
2016-06-28 00:03:20 -0500

    browsers talking to each other

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,7 @@ var socket = io();
 socket.on('connect',function(){
 	console.log('I am connected to socket.io server.');
 });
+
+socket.on('message',function(message){
+	console.log(message.text);
+});
